787221fa49b3fdfbbf9ca27292234348c771b12f
Caleb Allen <me@caleballen.dev>
2021-12-25 21:35:37 +0000

    Added gruvbox colour scheme

--- config.h
+++ config.h
@@ -1,21 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Nimbus Sans:size=9" };
+static const char dmenufont[]       = "Nimbus Sans:size=9";
+
+static const char col_dark_grey[]   = "#1c2121";
+static const char col_grey[]        = "#504945";
+static const char col_yellow[]      = "#fabd2f";
+static const char col_cyan[]        = "#83a598";
+static const char col_teal[]        = "#8ec07c";
+static const char col_red[]         = "#fb4934";
+static const char col_green[]       = "#b8bb26";
+static const char col_purple[]      = "#d3869b";
+static const char col_white[]       = "#ebdbb2";
+static const char col_orange[]         = "#fe8019";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_grey, col_dark_grey, col_dark_grey },
+	[SchemeSel]  = { col_dark_grey, col_cyan,  col_cyan  },
 };
 
 /* tagging */
@@ -57,7 +64,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark_grey, "-nf", col_white, "-sb", col_cyan, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {

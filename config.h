774366df14abc1a336f634e7b20c0858e2dc12a1
Caleb Allen <me@caleballen.dev>
2022-01-04 17:42:51 +0000

    patch: Added bar height

--- config.h
+++ config.h
@@ -11,6 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;   
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 20;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Nimbus Sans:size=9" };
 static const char dmenufont[]       = "Nimbus Sans:size=9";
 

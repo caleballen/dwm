001df3f8c9ce8e71576adf834496742b69511e43
Caleb Allen <me@caleballen.dev>
2022-05-21 21:44:19 +0100

    config update

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-
+#include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 4;        /* gaps between windows */
@@ -81,10 +81,24 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark_grey, "-nf", col_white, "-sb", col_cyan, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
+static const char *volumemute[] = { "amixer", "-D", "pulse", "set", "Master", "1+", "toggle",  NULL };
+static const char *volumeraise[] = { "amixer", "-D", "pulse", "sset", "Master", "5%+",  NULL };
+static const char *volumelower[] = { "amixer", "-D", "pulse", "sset", "Master", "5%-",  NULL };
+
+static const char *backlightraise[] = { "xbacklight", "-inc", "10",  NULL };
+static const char *backlightlower[] = { "xbacklight", "-dec", "10",  NULL };
+
+static const char *playerplaypause[] = { "playerctl", "play-pause",  NULL };
+static const char *playernext[] = { "playerctl", "next",  NULL };
+static const char *playerprevious[] = { "playerctl", "previous",  NULL };
+
+static const char *screenshot[] = { "gnome-screenshot", "-i", NULL };
+static const char *screenshot_clipboard[] = { "gnome-screenshot", "-ac", NULL };
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,			XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -92,20 +106,34 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-//	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_z,      zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_f,      togglefullscr,      {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ 0,							XF86XK_AudioMute,	spawn,	{.v = volumemute }},
+	{ 0,							XF86XK_AudioRaiseVolume,	spawn,	{.v = volumeraise}},
+	{ 0,							XF86XK_AudioLowerVolume,	spawn,	{.v = volumelower }},
+	{ 0,							XF86XK_MonBrightnessUp,	spawn,	{.v = backlightraise }},
+	{ 0,							XF86XK_MonBrightnessDown,	spawn,	{.v = backlightlower}},
+	{ 0,							XF86XK_AudioPlay,	spawn,	{.v = playerplaypause}},
+	{ 0,							XF86XK_AudioPause,	spawn,	{.v = playerplaypause}},
+	{ 0,							XF86XK_AudioNext,	spawn,	{.v = playernext}},
+	{ 0,							XF86XK_AudioPrev,	spawn,	{.v = playerprevious}},
+	{ Mod1Mask|ControlMask,			XK_Down,	spawn,	{.v = playerplaypause}},
+	{ Mod1Mask|ControlMask,			XK_Right,	spawn,	{.v = playernext}},
+	{ Mod1Mask|ControlMask,			XK_Left,	spawn,	{.v = playerprevious}},
+	{ 0,							XK_Print,	spawn,	{.v = screenshot}},
+	{ MODKEY|ShiftMask,				XK_s,		spawn,	{.v = screenshot_clipboard}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
